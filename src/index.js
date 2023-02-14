b0196e02d637a431515f70d305f1bc1c28bc3bb9
Sue Smith <sue@benormal.info>
2023-02-14 17:34:48 +0000

    Add source code from Fiddle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 /// <reference types="@fastly/js-compute" />
 
-addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+function handler(event) {
+  let clientGeo = event.client.geo
 
-async function handleRequest(event) {
-  return new Response("OK", { status: 200 });
+  let respBody = JSON.stringify({
+    as: {
+      name: clientGeo.as_name,
+      number: clientGeo.as_number,
+    },
+    geo: {
+      city: clientGeo.city,
+      country_code: clientGeo.country_code,
+      country_name: clientGeo.country_name,
+      gmt_offset: clientGeo.gmt_offset,
+    },
+  })
+
+  return new Response(respBody, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
 }
+
+// eslint-disable-next-line no-restricted-globals
+addEventListener("fetch", (event) => event.respondWith(handler(event)))
